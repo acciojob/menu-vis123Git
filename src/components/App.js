23e00b45071e245f54ab12e14f0814c36d40b093
VishalKumarSain <vishaldhoria007@gmail.com>
2024-09-11 22:48:09 +0530

    check test case

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import data from "../data";
 import Card from "./Card";
-import "../styles/App.css"
+import "../styles/App.css";
 
 function App() {
   const [menuItems, setMenuItems] = useState([]);
@@ -9,11 +9,11 @@ function App() {
 
   useEffect(() => {
     setMenuItems(data);
-    // Get unique categories including "all"
+    // Get unique categories including "All"
     const uniqueCategories = ["All", ...new Set(data.map((item) => {
-       const itm =  item.category
-       return itm[0].toUpperCase() + itm.slice(1).toLowerCase()
-}))];
+      const itm = item.category;
+      return itm[0].toUpperCase() + itm.slice(1).toLowerCase();
+    }))];
     setCategories(uniqueCategories);
   }, []);
 
@@ -21,30 +21,37 @@ function App() {
     if (category.toLowerCase() === "all") {
       setMenuItems(data);
     } else {
-      const newItems = data.filter((item) => item.category.toLowerCase() == category.toLowerCase());
+      const newItems = data.filter((item) => item.category.toLowerCase() === category.toLowerCase());
       setMenuItems(newItems);
     }
   };
 
   return (
-      <div className="menu section" id="main">
-        <div className="title">
-          <h2>Our Menu</h2>
-          <div className="underline"></div>
-        </div>
-        <div className="btn-container">
-          {categories.map((category, index) => (
-            <button type="button" className="filter-btn" data-test-id={`menu-item-${category.toLowerCase()}`} id={`filter-btn-${index}`} key={index} onClick={() => filterItems(category)}>
-              {category}
-            </button>
-          ))}
-        </div>
-        <div className="section-center">
-          {menuItems.map((item) => {
-            return <Card item={item} />;
-          })}
-        </div>
+    <div className="menu section" id="main">
+      <div className="title">
+        <h2>Our Menu</h2>
+        <div className="underline"></div>
       </div>
+      <div className="btn-container">
+        {categories.map((category, index) => (
+          <button
+            type="button"
+            className="filter-btn"
+            data-testid={`menu-item-${category.toLowerCase()}`}
+            id={`filter-btn-${index}`}
+            key={index}
+            onClick={() => filterItems(category)}
+          >
+            {category}
+          </button>
+        ))}
+      </div>
+      <div className="section-center">
+        {menuItems.map((item) => {
+          return <Card key={item.id} item={item} />;
+        })}
+      </div>
+    </div>
   );
 }
 
